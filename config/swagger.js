8b5a3a7967d06bafbde80192215906a044b099f6
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2025-06-17 20:48:14 +0100

    Centralized the swagger documentation inside the docs folder.

--- config/swagger.js
+++ config/swagger.js
@@ -1,45 +1,11 @@
-const swaggerJSDoc = require("swagger-jsdoc");
+const YAML = require('yamljs');
 const swaggerUI = require("swagger-ui-express");
 const logger = require("../utils/logger");
-const { constants } = require("../utils/constants");
 
-const PORT = process.env.PORT || constants.DEFAULT_PORT;
-
-const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Blog API",
-            version: "1.0.0",
-            description: "API documentation for the Blog Platform",
-        },
-        servers: [
-            {
-                url: `http://localhost:${PORT}`,
-            },
-        ],
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: "https",
-                    scheme: "bearer",
-                    bearerFormat: "JWT",
-                },
-            },
-        },
-        security: [
-            {
-                bearerAuth: [],
-            },
-        ],
-    },
-    apis: ["./routes/*.js", "./controllers/*.js"],
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+const swaggerDocument = YAML.load("docs/swagger.yaml");
 
 function setupSwagger(app) {
-    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
     logger.info("Swagger API setup!");
 }
 
