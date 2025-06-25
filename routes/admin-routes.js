863ead6d7ff5aa7e3c9cf12ddd727b0252045fd7
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2025-06-25 22:11:33 +0100

    Added the swagger documenation for the routes.

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -6,73 +6,9 @@ const { authRouteProtection } = require('../middleware/auth-middleware');
 
 router.use(authRouteProtection, authorizeRoles('admin'));
 
-/**
- * @swagger
- *  components:
- *      schema:
- *          User:
- *              type: object
- *              properties:
- *                  _id:
- *                      type: string
- *                  username:
- *                      type: string
- *                  email:
- *                      type: string
- *                  role:
- *                      type: string
- *                  createdAt:
- *                      type: string
- *                  updatedAt:
- *                      type: string
- */
-
-/**
- * @swagger
- * /users:
- *  get:
- *      summary: Gets all the registered users.
- *      description: This api is used to get all the registered users
- *      responses:
- *          200:
- *              description: The users were returned successfully.
- *              content:
- *                  application/json:
- *                      schema:
- *                          type: array
- *                          items:
- *                              $ref: '#components/schema/User'
- */
 router.get('/users', getAllUsers);
 
-/**
- * @swagger
- * /users/{userId}:
- *  delete:
- *      summary: Delete a user by their id.
- *      description: This api deletes a given user by their id.
- *      parameters:
- *          - in: path
- *            name: id
- *            required: true
- *            description: The user's uuid
- *            schema:
- *              type: string
- *      responses:
- *          200:
- *              description: The users was deleted successfully.
- *              content:
- *                  application/json:
- *                      schema:
- *                          type: object
- *                          properties:
- *                              message:
- *                                  type: string
- *                                  example: "User JohnD deleted successfully."
- *                          
- */
 router.delete('/users/:userId', deleteUser);
-
 
 router.patch('/users/:userId/role', updateUserRole);
 
