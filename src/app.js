08c0aabcb05c7be581467ec7d0be2fa375ca4578
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2025-07-20 21:00:25 +0100

    working on separating the swagger into different files

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const errorHandler = require('./middleware/error-handler');
 const cookieParser = require('cookie-parser');
 const limiter = require("./middleware/api-rate-limiter");
 const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
 
@@ -12,6 +13,13 @@ const app = express();
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+        credentials: true,
+    })
+);
 
 // Route setup
 app.use('/api/v1/', limiter);
@@ -21,6 +29,13 @@ app.use('/api/v1/admin', require('./api/v1/routes/admin-routes'));
 
 // Error handling middleware
 app.use(errorHandler);
+
+/**
+ * TODO: SORT OUT SWAGGER DOCS
+ * TODO: MAKE SURE API ROUTES WORK IN DOCS
+ * TODO: UPDATE README.md
+ * TODO: ADD TO PORTFOLIO
+ */
 
 
 module.exports = app;
